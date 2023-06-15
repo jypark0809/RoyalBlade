06945ed8cea1b23b47a392b2c8d069f13999bbfd
jypark0809 <jypark_0809@naver.com>
2023-06-15 20:26:39 +0900

    Real Last Commit

--- Assets/@Scripts/Managers/Managers.cs
+++ Assets/@Scripts/Managers/Managers.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.EditorTools;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Managers : MonoBehaviour
 {
